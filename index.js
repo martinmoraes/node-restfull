cf57ccc7347f4e85c69814838e8df13e8a724311
Martin Morães <martin.moraes@gmail.com>
2020-05-27 17:32:30 -0300

    feat: servidor http

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require ('http')
+
+let server = http.createServer((req, res) => {
+
+    console.log('URL: ', req.url)
+    console.log('METHODO: ', req.method)
+
+    res.end('OK')
+})
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Servidor rodando...')
+})
